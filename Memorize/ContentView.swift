96bae391f82dd9dd48cea9ec97152f259e5b00d0
Vasiliy Pivovarov <pivovarov.v.s@appfox.ru>
2021-05-17 00:08:41 +0400

    Finish 1st lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
